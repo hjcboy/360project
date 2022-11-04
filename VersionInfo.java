3712dfe8f2d310626c5b7d8a1659860e1ada781d
Deep <115367789+deepuh@users.noreply.github.com>
2022-11-04 14:18:05 -0700

    Add files via upload

---
+++ VersionInfo.java
@@ -0,0 +1,32 @@
+
+public class VersionInfo {
+	private final static String D1 = "Deep Singh";
+	private final static String D2 = "Deep Singh";
+	private final static String D3 = "Deep Singh";
+	private final static String D4 = "Deep Singh";
+	private final static String D5 = "Deep Singh";
+	private final static String myGithub = "https://github.com/Team-Leftovers-UWT/360project";
+	private final static String myVersion = "v.0.1.0";
+	
+	public static String getD1() {
+		return D1;
+	}
+	public static String getD2() {
+		return D2;
+	}
+	public static String getD3() {
+		return D3;
+	}
+	public static String getD4() {
+		return D4;
+	}
+	public static String getD5() {
+		return D5;
+	}
+	public static String getGitHub() {
+		return myGithub;
+	}
+	public static String getVersion() {
+		return myVersion;
+	}
+}
