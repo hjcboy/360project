207562da5e5c5327cc415e6bcd20648540d2c268
Fiblmao <115599969+Fiblmao@users.noreply.github.com>
2022-12-01 18:15:33 -0800

    Update AppGUI.java

--- AppGUI.java
+++ AppGUI.java
@@ -31,7 +31,10 @@ public class AppGUI extends JFrame {
 
     private void profilePanel() {
         panel = new JPanel();
+        panel.setSize(FRAME_SIZE);
+
         JPanel tempPanel = new JPanel();
+        Path userDir = Paths.get(filePath + "\\Users");
 
         /**
          * // For each profile, create a profile button. -----> IMPORT PROFILES. MAYBE USE FOR LOOP FOR THIS SECTION
@@ -42,13 +45,23 @@ public class AppGUI extends JFrame {
          *         // tempPanel.add(profileButton); // FOR EACH PROFILE, ADD THE BUTTON TO TEMP FRAME
          *         // panel.add(tempPanel, BorderLayout.CENTER);
          */
+        for (int i = 1; i < userCount + 1; i++) {
+            profileButton = new JButton("User " + i);
+            tempPanel.add(profileButton);
+        }
 
+        addProfile = new JButton("Create Profile");
+        addProfile.setFocusable(false);
 
         aboutButton = new JButton("About");
         aboutButton.setFocusable(false);
 
         panel.add(aboutButton, BorderLayout.SOUTH);
+        tempPanel.add(addProfile, BorderLayout.NORTH);
+        panel.add(tempPanel, BorderLayout.CENTER);
         frame.add(panel, BorderLayout.CENTER);
+        profileEvent();
+        addProfileEvent();
         addAboutEvent();
     }
 
@@ -134,8 +147,75 @@ public class AppGUI extends JFrame {
             }
         });
     }
+    
+    private void addProfileEvent() {
+        addProfile.addActionListener(event -> {
+            System.out.println("Create Profile button clicked.");
+            createProfilePanel();
+        });
+    }
+
+    /**
+     * createProfilePanel opens up a prompt asking the user for a
+     *                      username, email and determine its admin status
+     */
+    private void createProfilePanel() {
+        JFrame createFrame = new JFrame("Create a Profile"); // Creates a New Window for About Info.
+        createFrame.setVisible(true);
+        createFrame.setLayout(null);
+        createFrame.setSize(300,180);
+        createFrame.setLocationRelativeTo(null);
+
+        JTextField setUsername = new JTextField();
+        setUsername.setBounds(100,10,165,25);
+
+        JLabel user = new JLabel("Username");
+        user.setBounds(10,10, 80,25);
+
+        JTextField setEmail = new JTextField();
+        setEmail.setBounds(100,50,165,25);
+
+        JLabel userEmail = new JLabel("Email");
+        userEmail.setBounds(10, 50, 80, 25);
+
+        JCheckBox isAdmin = new JCheckBox();
+        isAdmin.setText("Admin Account");
+        isAdmin.setBounds(80, 80, 150, 25);
+        isAdmin.setFocusable(false);
+
+        JButton submit = new JButton("Submit");
+        submit.setBounds(80,110,150,25);
+
+        createFrame.add(user);
+        createFrame.add(setUsername);
+        createFrame.add(userEmail);
+        createFrame.add(setEmail);
+        createFrame.add(isAdmin);
+        createFrame.add(submit);
+
+        submit.addActionListener(event -> {
+            userCount++;
+            String username = setUsername.getText();
+            String email = setEmail.getText();
+            boolean adminStatus = false;
+            if (isAdmin.isSelected()) {
+                adminStatus = true;
+            }
+            Profile User = new Profile(username, email, adminStatus, userCount);
+            Profile.makeProfile(User.getUserName(), User.getEmail(), User.isAdmin(),User.getCount());
+            createFrame.dispose();
+            frame.dispose();
+            start();
+        });
+    }
 
     public static void main(String[] theArgs) {
+        try {
+            Files.createDirectories(Paths.get(System.getProperty("user.home") + "\\Documents\\LeftOverApp\\Users"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
         AppGUI theGUI = new AppGUI(); // Automatically starts.
     }
 
